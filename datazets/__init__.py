d4455d58236e51cfe18b7d413829870efdad086f
erdogant <erdogant@gmail.com>
2023-05-13 21:00:39 +0200

    0.1.1

--- datazets/__init__.py
+++ datazets/__init__.py
@@ -3,7 +3,7 @@ from datazets.datazets import get
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 # module level doc-string
 __doc__ = """
